cf70a8c2702591f8a2cdb68ed8cf29c8d9e05f82
foney <xuzhiyuan1994@qq.com>
2017-08-11 10:12:54 +0800

    proiect create

---
+++ app/src/main/java/com/foney/foneyweather/MainActivity.java
@@ -0,0 +1,13 @@
+package com.foney.foneyweather;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
