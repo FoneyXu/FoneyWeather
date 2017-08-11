b21c1b644e2a5bda23bd71890e389f382d8596b4
foney <xuzhiyuan1994@qq.com>
2017-08-11 10:43:11 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/foney/foneyweather/db/City.java
@@ -0,0 +1,48 @@
+package com.foney.foneyweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by foney on 2017/8/11.
+ * 城市实体类
+ */
+
+public class City extends DataSupport {
+
+    private int id;
+    private String cityName;
+    private String cityCode;
+    private int provinceId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public String getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(String cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+}
